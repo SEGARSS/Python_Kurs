02f5d50940578b800189153b182a0d0c5e32b0fb
SEGARS <vim_branet@mail.ru>
2023-04-22 00:21:10 +0500

    дз за 5й семинар

---
+++ DZ/Zadacha_05_00.py
@@ -0,0 +1,20 @@
+'''
+Задача 0.
+Напишите программу, 
+которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+'''
+def power(num, step):
+    if step == 0:
+        return 1
+    elif step % 2 == 0:
+        temp = power(num, step // 2)
+        return temp * temp
+    else:
+        return num * power(num, step-1)
+        
+num = int(input("Введите число: "))
+step = int(input("Введите степень: "))
+
+result = power(num, step)
+print(f"{num} в степени {step} равно {result}")
